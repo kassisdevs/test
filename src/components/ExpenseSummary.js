22018517528545d96b45eb99008ec28dd5f54552
kassisdevs <seogatorcompany@gmail.com>
2018-05-10 01:40:37 -0500

    added count

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
 import selectTotalExpenses from '../selectors/expenses-total';
+import visibleExpenses from '../selectors/expenses';
 
-export const ExpenseSammary = (props) => (
+export const ExpenseSammary = ({ expenseCount, expensesTotal }) => (
   <div>
   <p>
-  Viewing {props.expenseCount} expenses totaling {numeral(props.expensesTotal / 100).format('$0,0.00')}
+  Viewing {expenseCount} {expenseCount === 1 ? 'expense' : 'expenses'} totaling {numeral(expensesTotal / 100).format('$0,0.00')}
   </p>
   </div>
 );
 
-const mapToStateStore = (state) => ({
-  expenseCount: state.expenses.length,
-  expensesTotal: selectTotalExpenses(state.expenses)
-});
+const mapToStateStore = (state) => {
+  const visibleExpensestoShow = visibleExpenses(state.expenses, state.filters);
+  return ({
+    expenseCount: visibleExpensestoShow.length,
+    expensesTotal: selectTotalExpenses(visibleExpensestoShow)
+  });
+}
 
 export default connect(mapToStateStore)(ExpenseSammary);
